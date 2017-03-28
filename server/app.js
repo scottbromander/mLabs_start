dc42e8275062616881d82b5c4f45da9d10b42a29
Scott Bromander <scott@primeacademy.io>
2017-03-28 12:01:49 -0500

    initial commit

---
+++ server/app.js
@@ -0,0 +1,41 @@
+//General Modules
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var path = require("path");
+
+//Route Imports
+var employees = require("./routes/employee");
+
+//Database Variables
+var mongoose = require("mongoose");
+var mongoURI = "mongodb://localhost:27017/market";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+//If there is an error connecting to the database, let us know!
+MongoDB.on("error", function(err){
+  console.log("Mongo Connection Error :" + err);
+});
+
+//If we successfully hooked up to the database, let us know!
+MongoDB.once("open", function(){
+  console.log("Tots connected to Mongo, meow.");
+});
+
+//Set the port
+app.set("port", (process.env.PORT || 5000));
+
+//Middleware hookups
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("/server/public/"));
+
+//Routes
+app.use("/employees", employees);
+app.get("/", function(req,res){
+  res.sendFile(path.resolve("server/public/views/index.html"));
+});
+
+//Listen
+app.listen(app.get("port"), function(){
+    console.log("Listening on port: " + app.get("port"));
+});
---
+++ server/routes/employee.js
@@ -0,0 +1,81 @@
+var express = require("express");
+var router = express.Router();
+var mongoose = require("mongoose");
+
+// Defines HOW Documents will be saved to the Database
+var EmployeeSchema = mongoose.Schema({
+  name : String,
+  position: String,
+  salary: Number
+});
+
+/*
+  Employees - Is a reference to the collection when finding things in the DB,
+  Employees - Is a reference to the Schema, when we are saving things to the DB.
+*/
+var Employees = mongoose.model("Employees", EmployeeSchema);
+
+//GET employees
+router.get("/", function(req,res){
+  //Get all employees
+  Employees.find(function(err, allEmployees){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.send(allEmployees);
+  });
+});
+
+//Save a new employee
+router.post("/", function(req,res){
+  //Instance of the Model to be saved to the database
+  var employee = new Employees();
+  employee.name = req.body.name;
+  employee.position = req.body.position;
+  employee.salary = req.body.salary;
+  employee.save(function(err, savedEmployee){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+
+    res.send(savedEmployee);
+  });
+});
+
+/*
+  $.ajax({
+      type: "DELETE",
+      url: "/employees/" + id,
+      success stuff
+  });
+  $.ajax({
+      type: "DELETE",
+      data: data,
+      url: "/employees/",
+      success stuff
+  });
+*/
+
+//Delete an employee
+router.delete("/", function(req,res){
+  //Delete an employee
+  // { "id" : "83275019375918538?"}
+  var id = req.body.id;
+  Employees.findByIdAndRemove(id, function(err, deletedEmployee){
+    /*
+      if(undefined){} - False Value
+      if("Some Error Code"){} - True Value
+    */
+
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+
+    res.send(deletedEmployee);
+  });
+});
+
+module.exports = router;
